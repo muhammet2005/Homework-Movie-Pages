9168fbd6020e19bdcc9750536897c6e7e5fe20d9
muhammet2005 <maxmudgamer009@gmail.com>
2023-11-07 22:33:53 +0700

    first commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div id={"home"}>
             <div className="container">
-                <h1>Home</h1>
+                <h1 className={"home_title"}>Home</h1>
                 <div className="home">
                     <h1>Добро пожаловать..</h1>
                     <h3>Миллионы фильмов, сериалов и людей. Исследуйте сейчас.</h3>
